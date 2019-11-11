76919762aa111892e2497edb959f92405688f84d
Nebilam <49345234+Nebilam@users.noreply.github.com>
2019-11-11 11:59:22 +0100

    eerste commit

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+basic.forever(function () {
+    music.setTempo(pins.map(
+        Math.abs(input.acceleration(Dimension.Y)),
+        0,
+        1023,
+        60,
+        320
+    ))
+    music.playTone(input.lightLevel() * 25, music.beat(BeatFraction.Quarter))
+})
+basic.forever(function () {
+    led.plotBarGraph(
+        Math.abs(input.acceleration(Dimension.Y)),
+        1023
+    )
+})
